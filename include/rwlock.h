797cde576ac8604581d478b2eee9f74dceef0ade
Inho Song <mearrong123@gmail.com>
2020-09-27 17:36:56 +0900

    RW (cg) vs Sem_t lock only(cg)

--- include/rwlock.h
+++ include/rwlock.h
@@ -3,11 +3,8 @@
 
 #include <assert.h>
 #include <sched.h>
-
-
-#ifdef __linux__
 #include <semaphore.h>
-#endif
+
 #define Pthread_create(thread, attr, start_routine, arg) assert(pthread_create(thread, attr, start_routine, arg) == 0);
 #define Pthread_join(thread, value_ptr)                  assert(pthread_join(thread, value_ptr) == 0);
 
@@ -23,11 +20,10 @@
 #define Cond_signal(cond)                                assert(pthread_cond_signal(cond) == 0);
 #define Cond_wait(cond, mutex)                           assert(pthread_cond_wait(cond, mutex) == 0);
 
-#ifdef __linux__
 #define Sem_init(sem, value)                             assert(sem_init(sem, 0, value) == 0);
 #define Sem_wait(sem)                                    assert(sem_wait(sem) == 0);
 #define Sem_post(sem)                                    assert(sem_post(sem) == 0);
-#endif // __linux__
+
 
 //1. Coarse Grain Lock 
 sem_t CGlock;
